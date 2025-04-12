b6948b9c1dfdedd39d6665cc77e7f694e6fd7aac
yujining3827 <j_yujin_swu22@naver.com>
2025-04-12 18:15:29 +0900

    Initialize CURIO Spring Boot project

---
+++ src/main/java/team/backend/curio/CurioApplication.java
@@ -0,0 +1,13 @@
+package team.backend.curio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurioApplication.class, args);
+	}
+
+}
