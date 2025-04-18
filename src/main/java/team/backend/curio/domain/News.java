2c417adee91c758afe5724a7f9e5474137cff88c
yujining3827 <j_yujin_swu22@naver.com>
2025-04-19 03:46:29 +0900

    feat : News 엔티티 정의

--- src/main/java/team/backend/curio/domain/News.java
+++ src/main/java/team/backend/curio/domain/News.java
@@ -1,4 +1,48 @@
 package team.backend.curio.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "News")
 public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
+    private Long newsId;
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "summary_short")
+    private String summaryShort;
+
+    @Column(name = "summary_medium")
+    private String summaryMedium;
+
+    @Column(name = "summary_long")
+    private String summaryLong;
+
+    private String category;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    //private String newsKeyword;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "source_url") //원본 기사링크
+    private String sourceUrl;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Long getNewsId() {
+        return newsId;
+    }
 }
