54c0b03826aaa0f054c518c16dd7e2ae6403d87f
yujining3827 <j_yujin_swu22@naver.com>
2025-04-24 17:36:47 +0900

    NewsController ArticleController에 합치기

--- src/main/java/team/backend/curio/controller/ArticleController.java
+++ src/main/java/team/backend/curio/controller/ArticleController.java
@@ -1,9 +1,11 @@
 package team.backend.curio.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.backend.curio.domain.News;
 import team.backend.curio.dto.NewsDTO.NewsResponseDto;
 import team.backend.curio.dto.NewsDTO.RelatedNewsResponse;
 import team.backend.curio.service.NewsService;
@@ -29,5 +31,20 @@ public class ArticleController {
     public ResponseEntity<NewsResponseDto> getArticleHeadline(@PathVariable Long articleId) {
         NewsResponseDto newsResponse = newsService.getArticleHeadline(articleId); // NewsService에서 데이터 가져오기
         return ResponseEntity.ok(newsResponse); // 헤드라인과 이미지 URL 반환
+    }
+
+    // 뉴스 저장 API
+    @Operation(summary = "크롤링 뉴스 데이터 저장")
+    @PostMapping("/crawler")
+    public ResponseEntity<String> saveNews(@RequestBody List<News> newsList) {
+        System.out.println("API 호출됨!");
+        newsService.saveAllNews(newsList);
+        return ResponseEntity.ok("크롤링 뉴스 저장 성공!");
+    }
+
+    @Operation(summary = "News table 데이터 불러오기")
+    @GetMapping("/list")
+    public List<News> getNewsList() {
+        return newsService.getAllNews();
     }
 }
