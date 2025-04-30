29dee331b6c9820c7c9bbfa892906db11531c7aa
KwonChaeyoung02 <loolcy0135@gmail.com>
2025-04-30 14:00:41 +0900

    Revert "feat: 홈화면 뉴스레터 신청 api"

--- src/main/java/team/backend/curio/repository/UserRepository.java
+++ src/main/java/team/backend/curio/repository/UserRepository.java
@@ -2,13 +2,8 @@ package team.backend.curio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.backend.curio.domain.users;
-import java.util.List;
-
 
 public interface UserRepository extends JpaRepository<users, Long> {
     // 추가적인 쿼리 메서드가 필요하면 여기에 작성
-
-    // 뉴스레터 신청 상태가 1이고, 수신 이메일이 설정된 사용자 조회
-    List<users> findByNewsletterStatusAndNewsletterEmailNotNull(int status);
-
 }
+
