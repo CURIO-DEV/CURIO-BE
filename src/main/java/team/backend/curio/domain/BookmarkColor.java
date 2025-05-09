406c66a330271b0583884c44724073a7e30a7c51
yujining3827 <j_yujin_swu22@naver.com>
2025-05-10 03:34:28 +0900

    feat : bookmarkcolor 대소문자 무시하도록 수정

--- src/main/java/team/backend/curio/domain/BookmarkColor.java
+++ src/main/java/team/backend/curio/domain/BookmarkColor.java
@@ -1,11 +1,18 @@
 package team.backend.curio.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum BookmarkColor {
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    NAVY,
-    PURPLE
+    RED, ORANGE, YELLOW, GREEN, BLUE, NAVY, PURPLE;
+
+    @JsonCreator
+    public static BookmarkColor from(String value) {
+        return BookmarkColor.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toLowerCase();
+    }
 }
