91459ce4fc28ef7d0acbd3b0d6ae272e3c2f9c25
권채영 <kwonchaeyeong@gwonchaeyeong-ui-MacBookPro.local>
2025-05-28 13:27:26 +0900

    feat: WebConfig CORS 설정 수정

--- src/main/java/team/backend/curio/config/WebConfig.java
+++ src/main/java/team/backend/curio/config/WebConfig.java
@@ -1,23 +1,21 @@
 package team.backend.curio.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // 혹은 운영 도메인도 같이
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://www.curi-o.site"
+                )
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600); // Preflight 요청 캐싱 시간 (선택)
     }
 }
