bbe11c7e00d5eb25c8c3c3c69ec1f2e64a3274bb
yujining3827 <j_yujin_swu22@naver.com>
2025-06-04 10:14:56 +0900

    fix : NoArgsConstructor 추가

--- src/main/java/team/backend/curio/dto/BookmarkDTO/NewsAddBookmarkDto.java
+++ src/main/java/team/backend/curio/dto/BookmarkDTO/NewsAddBookmarkDto.java
@@ -1,9 +1,10 @@
 package team.backend.curio.dto.BookmarkDTO;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-
+@NoArgsConstructor
 public class NewsAddBookmarkDto {
     private Long articleId;
 }
