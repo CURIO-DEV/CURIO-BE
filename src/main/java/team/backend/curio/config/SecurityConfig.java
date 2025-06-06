b8b6df29da1325b1883f20641de60c33c5bb1700
yujining3827 <j_yujin_swu22@naver.com>
2025-06-06 23:23:36 +0900

    fix : https://www.curi-o.site 경로 추가

--- src/main/java/team/backend/curio/config/SecurityConfig.java
+++ src/main/java/team/backend/curio/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://curi-o.site"
+                "https://www.curi-o.site"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
