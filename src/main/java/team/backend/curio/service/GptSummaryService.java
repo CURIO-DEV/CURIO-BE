3bab9a632600d9f02a99e07fe413d32de0c9c478
yujining3827 <j_yujin_swu22@naver.com>
2025-06-17 21:50:47 +0900

    fix : gpt 모델 변경

--- src/main/java/team/backend/curio/service/GptSummaryService.java
+++ src/main/java/team/backend/curio/service/GptSummaryService.java
@@ -42,7 +42,7 @@ public class GptSummaryService {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, Object> requestBody = Map.of(
-                "model", "gpt-3.5-turbo",
+                "model", "gpt-4o",
                 "temperature", 0,
                 "messages", List.of(
                         Map.of("role", "user", "content", fullPrompt)
@@ -59,45 +59,6 @@ public class GptSummaryService {
 
         List<Map<String, Object>> choices = (List<Map<String, Object>>) response.getBody().get("choices");
         return (String) ((Map<String, Object>) choices.get(0).get("message")).get("content");
-    }
-
-    public List<KeywordDto> callGptForKeywordExtraction(String prompt) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(apiKey);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        Map<String, Object> requestBody = Map.of(
-                "model", "gpt-4o",
-                "messages", List.of(
-                        Map.of("role", "user", "content", prompt)
-                )
-        );
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(
-                "https://api.openai.com/v1/chat/completions",
-                entity,
-                Map.class
-        );
-
-        List<Map<String, Object>> choices = (List<Map<String, Object>>) response.getBody().get("choices");
-        String gptResponseJson = (String) ((Map<String, Object>) choices.get(0).get("message")).get("content");
-
-
-        System.out.println("GPT 응답 원문: " + gptResponseJson);
-
-        // JSON 파싱
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(gptResponseJson, new TypeReference<List<KeywordDto>>() {});
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-
     }
 
 }
